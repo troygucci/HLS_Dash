1991ec157692c53e9d3ec9395ea57b8e1f23a2d4
troygucci <61523900+troygucci@users.noreply.github.com>
2025-10-01 16:48:35 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const App: React.FC = () => {
+  const styles = {
+    container: {
+      padding: '20px',
+      backgroundColor: '#f0f0f0',
+      margin: '20px'
+    },
+    heading: {
+      color: 'blue',
+      textAlign: 'center' as const
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello React!</h1>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>This is my React App!</h1>
+      <p>If you can see this, React is working!</p>
     </div>
   );
 };
